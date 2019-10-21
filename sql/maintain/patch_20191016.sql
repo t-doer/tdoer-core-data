dd91577856f5ffbf3749906d5ed25bc25c3a887c
Htinker Hu <htinker@163.com>
2019-10-22 00:33:50 +0800

    Adjust fw_action_ext tables

--- sql/maintain/patch_20191016.sql
+++ sql/maintain/patch_20191016.sql
@@ -38,6 +38,7 @@ create table fw_action_ext
 (
     ID                   bigint not null auto_increment comment '扩展ID',
     ACTION_ID            bigint not null comment '操作ID',
+    PAGE_ID              bigint not null comment '操作所属页面ID',
     APPLICATION_ID       bigint not null comment '操作所属应用ID',
     PRODUCT_ID           bigint not null default 0 comment '扩展属性：给特定产品的扩展，默认：0',
     CLIENT_ID            bigint not null default 0 comment '扩展属性：给特定产品端的扩展，默认：0',
@@ -59,7 +60,8 @@ create table fw_action_ext
  * Index list
  */
 create unique index idx_fw_action_ext_1 on fw_action_ext(ACTION_ID, PRODUCT_ID, CLIENT_ID, TENANT_ID, CONTEXT_PATH);
-create index idx_fw_action_ext_2 on fw_action_ext(APPLICATION_ID, PRODUCT_ID, CLIENT_ID, TENANT_ID, CONTEXT_PATH);
+create index idx_fw_action_ext_2 on fw_action_ext(PAGE_ID, APPLICATION_ID, PRODUCT_ID, CLIENT_ID, TENANT_ID,
+                                                  CONTEXT_PATH);
 
 drop table if exists fw_action_method;
 
